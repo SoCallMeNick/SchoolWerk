0e89810834425292c2ff1d54483bc84d7e6ce508
Nick Camijn <nickcamijn@hotmail.com>
2015-11-24 14:58:26 +0100

    BEGIN HIER v0.2

--- Project 2/index.php
+++ Project 2/index.php
@@ -1,19 +1,69 @@
 <?php
-include_once( "./lib/class.TemplatePower.inc.php" );
+include_once("./lib/class.TemplatePower.inc.php");
 
-
-$tpl = new TemplatePower( "./template.tpl" );
+$tpl = new TemplatePower("./template.tpl");
 $tpl->prepare();
 
+$choice = null;
+$rnd = null;
 
-if (empty($_POST)) {
-    $tpl->newBlock( "default" );
-    $tpl->assign( "name", "if" );
+if (!empty($_POST['rock'])) {
+    $choice = "rock";
+    $tpl->newBlock("rock");
+    $tpl->assign("name", "Rock");
+} elseif (!empty($_POST['paper'])) {
+    $choice = "paper";
+    $tpl->newBlock("paper");
+    $tpl->assign("name", "Paper");
+} elseif (!empty($_POST['scissor'])) {
+    $choice = "scissor";
+    $tpl->newBlock("scissor");
+    $tpl->assign("name", "Scissor");
+} else {
+    $tpl->newBlock("noPost");
+    $tpl->assign("name", "No Post");
 }
-else {
-    $tpl->newBlock( "post" );
-    $tpl->assign( "name", "else" );
+
+$tpl->newBlock("options");
+
+if (!empty($_POST)) {
+    $rnd = rand(1, 3);
+    if ($rnd == 1) {
+        echo "$choice > Rock <br />";
+        if ($choice = "rock") {
+            echo "rock vs. rock. Draw.";
+        }
+        elseif ($choice = "paper") {
+            echo "paper beats rock. Win.";
+        }
+        elseif ($choice = "scissor") {
+            echo "rock crushes scissor. Lose.";
+        }
+    }
+    if ($rnd == 2) {
+        echo "$choice > Paper  <br />";
+        if ($choice = "rock") {
+            echo "paper wraps around rock. lose.";
+        }
+        elseif ($choice = "paper") {
+            echo "paper vs. paper. Draw.";
+        }
+        elseif ($choice = "scissor") {
+            echo "scissor cuts paper. Win.";
+        }
+    }
+    if ($rnd == 3) {
+        echo "$choice > scissor <br />";
+        if ($choice = "rock") {
+            echo "rock crushes scissor. Win.";
+        }
+        elseif ($choice = "paper") {
+            echo "scissor cuts paper. Lose.";
+        }
+        elseif ($choice = "scissor") {
+            echo "scissor vs. scissor. Draw.";
+        }
+    }
 }
-$tpl->gotoBlock('_ROOT');
 
 $tpl->printToScreen();
--- Project 2/template.tpl
+++ Project 2/template.tpl
@@ -3,25 +3,45 @@
 <head>
     <meta charset="UTF-8">
 
-    <link href="css/normalize.css" rel="stylesheet" type="text/css" />
-    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <link href="css/normalize.css" rel="stylesheet" type="text/css"/>
+    <link href="css/style.css" rel="stylesheet" type="text/css"/>
 
     <title>Rock, Paper, Scissor</title>
 </head>
 
 <body>
-
-<!-- START BLOCK : post -->
+<!-- START BLOCK : noPost -->
 <h1>Rock, Paper, Scissor</h1>
 {name}
 
-<!-- END BLOCK : post -->
+<!-- END BLOCK : noPost -->
 
-<!-- START BLOCK : default -->
+<!-- START BLOCK : rock -->
 <h1>Rock, Paper, Scissor</h1>
 {name}
 
-<!-- END BLOCK : standard -->
+<!-- END BLOCK : rock -->
+
+<!-- START BLOCK : paper -->
+<h1>Rock, Paper, Scissor</h1>
+{name}
+
+<!-- END BLOCK : paper -->
+
+<!-- START BLOCK : scissor -->
+<h1>Rock, Paper, Scissor</h1>
+{name}
+
+<!-- END BLOCK : scissor -->
+
+<!-- START BLOCK : options -->
+<form action="./" method="POST" enctype="multipart/form-data">
+    <input type="submit" name="rock" value="Rock">
+    <input type="submit" name="paper" value="Paper">
+    <input type="submit" name="scissor" value="Scissor">
+</form>
+
+<!-- END BLOCK : options -->
 
 
 </body>
