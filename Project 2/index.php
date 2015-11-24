e8a7b276065cd8a5f4b821651328e159cdd2e334
Nick Camijn <nickcamijn@hotmail.com>
2015-11-24 17:31:13 +0100

    BEGIN HIER v0.2

--- Project 2/index.php
+++ Project 2/index.php
@@ -15,7 +15,8 @@ if (!empty($_POST['rock'])) {
     $choice = "paper";
     $tpl->newBlock("paper");
     $tpl->assign("name", "Paper");
-} elseif (!empty($_POST['scissor'])) {
+} elseif
+(!empty($_POST['scissor'])) {
     $choice = "scissor";
     $tpl->newBlock("scissor");
     $tpl->assign("name", "Scissor");
@@ -24,46 +25,42 @@ if (!empty($_POST['rock'])) {
     $tpl->assign("name", "No Post");
 }
 
-$tpl->newBlock("options");
 
 if (!empty($_POST)) {
     $rnd = rand(1, 3);
+    echo $rnd;
     if ($rnd == 1) {
         echo "$choice > Rock <br />";
-        if ($choice = "rock") {
+        if ($choice == "rock") {
             echo "rock vs. rock. Draw.";
-        }
-        elseif ($choice = "paper") {
+        } elseif ($choice == "paper") {
             echo "paper beats rock. Win.";
-        }
-        elseif ($choice = "scissor") {
+        } elseif ($choice == "scissor") {
             echo "rock crushes scissor. Lose.";
         }
     }
     if ($rnd == 2) {
         echo "$choice > Paper  <br />";
-        if ($choice = "rock") {
+        if ($choice == "rock") {
             echo "paper wraps around rock. lose.";
-        }
-        elseif ($choice = "paper") {
+        } elseif ($choice == "paper") {
             echo "paper vs. paper. Draw.";
-        }
-        elseif ($choice = "scissor") {
+        } elseif ($choice == "scissor") {
             echo "scissor cuts paper. Win.";
         }
     }
     if ($rnd == 3) {
         echo "$choice > scissor <br />";
-        if ($choice = "rock") {
+        if ($choice == "rock") {
             echo "rock crushes scissor. Win.";
-        }
-        elseif ($choice = "paper") {
+        } elseif ($choice == "paper") {
             echo "scissor cuts paper. Lose.";
-        }
-        elseif ($choice = "scissor") {
+        } elseif ($choice == "scissor") {
             echo "scissor vs. scissor. Draw.";
         }
     }
 }
+
+$tpl->newBlock("options");
 
 $tpl->printToScreen();
